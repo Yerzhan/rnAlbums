49addb8030a6b031530869edde94e81d13129137
sardieri <sardieri@gmail.com>
2018-12-25 22:45:12 +0300

    log out implemented

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, SafeAreaView} from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common';
+import { Header, Button, Spinner } from './src/components/common';
 import keys from './config/keys'
 import LoginForm from './src/components/LoginForm';
 
@@ -21,6 +21,8 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount(){
     firebase.initializeApp({
       apiKey: keys.apiKey,
@@ -30,13 +32,37 @@ export default class App extends Component {
       storageBucket: keys.storageBucket,
       messagingSenderId: keys.storageBucket
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
+
+  renderContent(){
+    switch(this.state.loggedIn){
+      case true: {
+        return (
+          <Button onPress={() => { firebase.auth().signOut() }}>
+            Log Out
+          </Button>
+        );
+      }
+      case false: 
+        return <LoginForm />;
+      default: 
+        return <Spinner />;
+    }
   }
 
   render() {
     return (
       <SafeAreaView style={{flex: 1, backgroundColor: '#F8F8F8'}}>
         <Header headerText={'Authentication'}/>
-        <LoginForm />
+        {this.renderContent()}
       </SafeAreaView>
     );
   }
