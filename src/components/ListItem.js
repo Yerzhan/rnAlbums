9f0a5baedd403dbb33e76bc2125d4d124c2c4603
sardieri <sardieri@gmail.com>
2018-12-27 19:17:33 +0300

    Displayed simple items list

---
+++ src/components/ListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
+
+class ListItem extends React.Component {
+  render(){
+    const { titleStyle } = styles;
+
+    return (
+      <CardSection>
+        <Text style={titleStyle}>{this.props.library.title}</Text>
+      </CardSection>
+    );
+  }
+};
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+}
+
+export default ListItem;
--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ListView, Text } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class TechStack extends React.Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(library) {
+    return <ListItem library={library} />
+  }
+
   render(){
     return (
-      <View />
+      <ListView 
+        dataSource={this.dataSource} 
+        renderRow={this.renderRow}
+      />
     );
   }
 }
